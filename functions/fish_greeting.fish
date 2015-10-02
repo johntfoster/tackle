557b856fcedae478a62f69cd52f69b0ae9031ba6
John Foster <johntfosterjr@gmail.com>
2015-10-01 20:39:16 -0500

    Update fish greeting

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,7 +1,6 @@
 function fish_greeting
-	_logo
-echo -e "Kernel Information: " (uname -smr)
-echo -e (fish -v)
-echo -ne "Uptime: "; uptime
-echo -ne "Server time is: "; date
+    echo -e "Kernel Information: " (uname -smr)
+    echo -ne (fish -v)
+    echo -ne "Uptime: "; uptime
+    echo -ne "Server time is: "; date
 end
